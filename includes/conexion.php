928c427327396dff6fd194823e574731778861ab
Robin Ramirez Legarda <easycryptocoins4free@gmail.com>
2022-03-18 19:40:18 -0500

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,17 +1,33 @@
-<?php
-try{
-$pdo = new PDO('mysql:host=localhost;dbname=easyeglp_prueba','easyeglp_prueba','Ld_HkcckQm3j1#@.Kl2');
-//echo 'conectado';
-} catch (PDOException $e){
-print "¡Error!: " . $e->getMessage() . "<br/>";
-die();
-}
-$servername = "localhost";
-$dBusername = "easyeglp_prueba";
-$dBpassword = "Ld_HkcckQm3j1#@.Kl2";
-$dBname = "easyeglp_prueba";
-$conn = mysqli_connect($servername, $dBusername, $dBpassword, $dBname);
-if(!$conn) {
-die("Connection To Database Failed: ".mysql_connect_error());
-}
+p<?php
+
+try{
+
+$pdo = new PDO('mysql:host=localhost;dbname=easycryptos','root','');
+
+//echo 'conectado';
+
+} catch (PDOException $e){
+
+print "¡Error!: " . $e->getMessage() . "<br/>";
+
+die();
+
+}
+
+$servername = "localhost";
+
+$dBusername = "root";
+
+$dBpassword = "";
+
+$dBname = "easycryptos";
+
+$conn = mysqli_connect($servername, $dBusername, $dBpassword, $dBname);
+
+if(!$conn) {
+
+die("Connection To Database Failed: ".mysql_connect_error());
+
+}
+
 ?>
